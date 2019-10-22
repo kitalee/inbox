2bcc833b2caa53c5bd3d351efb2295e6623bb559
DongGeon Lee <donggeon.lee@keymedia.com>
2019-10-22 17:58:56 +1100

    mocha test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,3 +2,32 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+// mocha function test
+
+// class Car {
+//     park(){
+//         return 'stopped';
+//     }
+//     drive(){
+//         return 'vroom';
+//     }
+// }
+//
+// let car;
+// // 매 it function 전에 호출됨 / initialization
+// beforeEach(() => {
+//     car = new Car();
+// });
+//
+// describe('Car', () => {
+//     it('car park', () => {
+//         // const car = new Car();
+//         assert.equal(car.park(), 'stopped');
+//     });
+//
+//     it('car drive', () => {
+//         // const car = new Car();
+//         assert.equal(car.drive(), 'vroom');
+//     });
+// });
