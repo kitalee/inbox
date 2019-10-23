2a27325ee8bdaa3c23fea67bb85f70365f945689
DongGeon Lee <donggeon.lee@keymedia.com>
2019-10-23 18:01:11 +1100

    48. Asserting Deployment

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -26,7 +26,8 @@ beforeEach(async () => {
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+        //console.log(inbox);
         //console.log(accounts);
     });
 });
