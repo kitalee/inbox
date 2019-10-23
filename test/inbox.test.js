f17a4329af635f3307043b0bfaa2f09c67fb34a0
DongGeon Lee <donggeon.lee@keymedia.com>
2019-10-23 12:23:05 +1100

    46.deploy test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,12 +2,19 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 // mocha function test
 beforeEach(async () => {
     // get a list of all accounts (Using async await syntax) / much simpler
     accounts = await web3.eth.getAccounts();
+
+    // use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['hi there'] })
+        .send({ from: accounts[0], gas: '1000000' });
 });
 
 // beforeEach(() => {
@@ -19,7 +26,8 @@ beforeEach(async () => {
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
+        //console.log(accounts);
     });
 });
 
